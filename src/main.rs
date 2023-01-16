f1c3a7ff50e86625ea13a325b1919aeaa08b0123
SebastianNeiswanger <31674857+SebastianNeiswanger@users.noreply.github.com>
2023-01-16 11:55:57 -0500

    changed messages

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
                 let h = std::thread::spawn(move || loop {
                     let context = zmq::Context::new();
                     let p = context.socket(zmq::REQ).unwrap();
-                    p.connect("tcp://proxy.cusmartevents.com:9951").unwrap();
+                    p.connect("tcp://localhost:9951").unwrap();
                     println!("sending");
                     p.send(format!("heartbeat 00:00:00:00:00:00").as_bytes(), 0)
                         .unwrap();
@@ -27,19 +27,27 @@ fn main() {
         "-s" => {
             let context = zmq::Context::new();
             let sub = context.socket(zmq::REP).unwrap();
-            sub.connect("tcp://proxy.cusmartevents.com:9950").unwrap();
+            sub.connect("tcp://localhost:9950").unwrap();
             let mut msg = zmq::Message::new();
             loop {
                 println!("waiting for message...");
                 sub.recv(&mut msg, 0).unwrap();
-                dbg!(msg.as_str().unwrap());
-                sub.send("ACK".as_bytes(), 0).unwrap();
+                match msg.as_str() {
+                    Some(m) => {
+                        dbg!(msg.as_str().unwrap());
+                        sub.send(format!("ACK {}", &m[10..]).as_bytes(), 0).unwrap();
+                    },
+                    None => {
+                        dbg!("recieved None");
+                        sub.send("ACK".as_bytes(), 0).unwrap();
+                    }
+                }
             }
         }
         "-l" => {
             let context = zmq::Context::new();
             let sub = context.socket(zmq::PAIR).unwrap();
-            sub.connect("tcp://proxy.cusmartevents.com:9952").unwrap();
+            sub.connect("tcp://localhost:9952").unwrap();
             let mut msg = zmq::Message::new();
             println!("waiting to log messages...");
             loop {
@@ -47,7 +55,7 @@ fn main() {
                 match msg.as_str() {
                     Some(m) => {
                         if !m.trim().is_empty() {
-                            println!("{}: {}", Local::now().format("%Y-%m-%d %H:%M:%S").to_string(), m);
+                            println!("{}: {}", Local::now().format("%Y-%m-%d %H:%M:%S.%3f").to_string(), m);
                         } 
                     },
                     None => (),
