96e9e780f9a4a933bc762829989ed5c5d55100cc
spbills <spencebills0@gmail.com>
2023-01-17 15:27:31 -0500

    Added env reading

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
 use chrono::{self, Local};
+use std::env;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
+    let parsed_url = env::var("HEARTBEAT_URL").unwrap_or("localhost".to_string());
+
+    println!("{parsed_url}");
 
     match args[1].as_str() {
         "-p" => {
-            let mut handles = Vec::new();
             for _i in 0..10 {
-                let h = std::thread::spawn(move || loop {
+                loop {
                     let context = zmq::Context::new();
                     let p = context.socket(zmq::REQ).unwrap();
-                    p.connect("tcp://localhost:9951").unwrap();
+                    p.connect(&format!("tcp://{}:9951", parsed_url.clone()))
+                        .unwrap();
                     println!("sending");
-                    p.send(format!("heartbeat 00:00:00:00:00:00").as_bytes(), 0)
+                    p.send(format!("heartbeat 00:00:00:00:00:0{}", _i).as_bytes(), 0)
                         .unwrap();
                     std::thread::sleep(std::time::Duration::from_millis(1000));
-                });
-                handles.push(h);
-            }
-
-            for h in handles {
-                h.join().unwrap();
+                }
             }
         }
         "-s" => {
             let context = zmq::Context::new();
             let sub = context.socket(zmq::REP).unwrap();
-            sub.connect("tcp://localhost:9950").unwrap();
+            sub.connect(&format!("tcp://{}:9950", parsed_url).clone())
+                .unwrap();
             let mut msg = zmq::Message::new();
             loop {
                 println!("waiting for message...");
@@ -36,7 +36,7 @@ fn main() {
                     Some(m) => {
                         dbg!(msg.as_str().unwrap());
                         sub.send(format!("ACK {}", &m[10..]).as_bytes(), 0).unwrap();
-                    },
+                    }
                     None => {
                         dbg!("recieved None");
                         sub.send("ACK".as_bytes(), 0).unwrap();
@@ -47,7 +47,8 @@ fn main() {
         "-l" => {
             let context = zmq::Context::new();
             let sub = context.socket(zmq::PAIR).unwrap();
-            sub.connect("tcp://localhost:9952").unwrap();
+            sub.connect(&format!("tcp://{}:9952", parsed_url.clone()))
+                .unwrap();
             let mut msg = zmq::Message::new();
             println!("waiting to log messages...");
             loop {
@@ -55,9 +56,13 @@ fn main() {
                 match msg.as_str() {
                     Some(m) => {
                         if !m.trim().is_empty() {
-                            println!("{}: {}", Local::now().format("%Y-%m-%d %H:%M:%S.%3f").to_string(), m);
-                        } 
-                    },
+                            println!(
+                                "{}: {}",
+                                Local::now().format("%Y-%m-%d %H:%M:%S.%3f").to_string(),
+                                m
+                            );
+                        }
+                    }
                     None => (),
                 }
             }
